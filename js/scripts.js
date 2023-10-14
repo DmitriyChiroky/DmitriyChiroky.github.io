cf71e8dc5a73eb405e1ea90253a6a4bde130a0e1
DmitriyChiroky <dimonnew777@gmail.com>
2023-10-14 16:31:19 +0300

    clear

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($) {
-    $('.s-body__link').on('click', function(event) {
-        event.preventDefault();
-        $('.s-body-info').addClass('active')
-    });
-});
